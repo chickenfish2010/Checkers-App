d1e78dd2a4a25fe0283df53ea3f003337d59265f
unknown <cpetty1@SI-20102.harding.edu>
2014-11-11 15:33:19 -0600

    Added checkers board

--- CheckersApp/Properties/AssemblyInfo.cs
+++ CheckersApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Checkers-App")]
+[assembly: AssemblyTitle("CheckersApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Checkers-App")]
+[assembly: AssemblyProduct("CheckersApp")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
