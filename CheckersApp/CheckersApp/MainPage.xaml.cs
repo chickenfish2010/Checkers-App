46a1b189472a9bc84ea1a0af99dec65f4338cb4e
unknown <cpetty1@SI-20103.harding.edu>
2014-12-02 12:32:59 -0600

    Most of the files from before were missing, so restarted with just the main page. Implemented click function and began work on move validation.

--- CheckersApp/CheckersApp/MainPage.xaml.cs
+++ CheckersApp/CheckersApp/MainPage.xaml.cs
@@ -27,6 +27,10 @@ namespace CheckersApp
         private Windows.UI.Xaml.Shapes.Rectangle moveFromSpace = null;
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        char[] startingSpace = new char[2];
+        char[] destinationSpace = new char[2];
+        int firstClick = 0;
+        bool northPlayersTurn = true;
 
         /// <summary>
         /// This can be changed to a strongly typed view model.
@@ -106,143 +110,427 @@ namespace CheckersApp
 
         private void Space_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            if (moveFromSpace == null)
-            {
-                moveFromSpace = (Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource;
+            if(firstClick == 0)
+            {            
+                if (sender == _00)
+                {
+                    startingSpace[0] = '0';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _01)
+                {
+                    startingSpace[0] = '0';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _02)
+                {
+                    startingSpace[0] = '0';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _03)
+                {
+                    startingSpace[0] = '0';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _10)
+                {
+                    startingSpace[0] = '1';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _11)
+                {
+                    startingSpace[0] = '1';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _12)
+                {
+                    startingSpace[0] = '1';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _13)
+                {
+                    startingSpace[0] = '1';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _20)
+                {
+                    startingSpace[0] = '2';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _21)
+                {
+                    startingSpace[0] = '2';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _22)
+                {
+                    startingSpace[0] = '2';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _23)
+                {
+                    startingSpace[0] = '2';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _30)
+                {
+                    startingSpace[0] = '3';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _31)
+                {
+                    startingSpace[0] = '3';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _32)
+                {
+                    startingSpace[0] = '3';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _33)
+                {
+                    startingSpace[0] = '3';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _40)
+                {
+                    startingSpace[0] = '4';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _41)
+                {
+                    startingSpace[0] = '4';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _42)
+                {
+                    startingSpace[0] = '4';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _43)
+                {
+                    startingSpace[0] = '4';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _50)
+                {
+                    startingSpace[0] = '5';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _51)
+                {
+                    startingSpace[0] = '5';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _52)
+                {
+                    startingSpace[0] = '5';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _53)
+                {
+                    startingSpace[0] = '5';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _60)
+                {
+                    startingSpace[0] = '6';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _61)
+                {
+                    startingSpace[0] = '6';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _62)
+                {
+                    startingSpace[0] = '6';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _63)
+                {
+                    startingSpace[0] = '6';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
+                else if (sender == _70)
+                {
+                    startingSpace[0] = '7';
+                    startingSpace[1] = '0';
+                    firstClick = 1;
+                }
+                else if (sender == _71)
+                {
+                    startingSpace[0] = '7';
+                    startingSpace[1] = '1';
+                    firstClick = 1;
+                }
+                else if (sender == _72)
+                {
+                    startingSpace[0] = '7';
+                    startingSpace[1] = '2';
+                    firstClick = 1;
+                }
+                else if (sender == _73)
+                {
+                    startingSpace[0] = '7';
+                    startingSpace[1] = '3';
+                    firstClick = 1;
+                }
             }
-            else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space1)
+            else if (firstClick == 1)
             {
-                Debug.WriteLine(e.OriginalSource.ToString());
+                if (sender == _00)
+                {
+                    destinationSpace[0] = '0';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _01)
+                {
+                    destinationSpace[0] = '0';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _02)
+                {
+                    destinationSpace[0] = '0';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _03)
+                {
+                    destinationSpace[0] = '0';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _10)
+                {
+                    destinationSpace[0] = '1';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _11)
+                {
+                    destinationSpace[0] = '1';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _12)
+                {
+                    destinationSpace[0] = '1';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _13)
+                {
+                    destinationSpace[0] = '1';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _20)
+                {
+                    destinationSpace[0] = '2';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _21)
+                {
+                    destinationSpace[0] = '2';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _22)
+                {
+                    destinationSpace[0] = '2';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _23)
+                {
+                    destinationSpace[0] = '2';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _30)
+                {
+                    destinationSpace[0] = '3';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _31)
+                {
+                    destinationSpace[0] = '3';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _32)
+                {
+                    destinationSpace[0] = '3';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _33)
+                {
+                    destinationSpace[0] = '3';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _40)
+                {
+                    destinationSpace[0] = '4';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _41)
+                {
+                    destinationSpace[0] = '4';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _42)
+                {
+                    destinationSpace[0] = '4';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _43)
+                {
+                    destinationSpace[0] = '4';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _50)
+                {
+                    destinationSpace[0] = '5';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _51)
+                {
+                    destinationSpace[0] = '5';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _52)
+                {
+                    destinationSpace[0] = '5';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _53)
+                {
+                    destinationSpace[0] = '5';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _60)
+                {
+                    destinationSpace[0] = '6';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _61)
+                {
+                    destinationSpace[0] = '6';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _62)
+                {
+                    destinationSpace[0] = '6';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _63)
+                {
+                    destinationSpace[0] = '6';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
+                else if (sender == _70)
+                {
+                    destinationSpace[0] = '7';
+                    destinationSpace[1] = '0';
+                    firstClick = 0;
+                }
+                else if (sender == _71)
+                {
+                    destinationSpace[0] = '7';
+                    destinationSpace[1] = '1';
+                    firstClick = 0;
+                }
+                else if (sender == _72)
+                {
+                    destinationSpace[0] = '7';
+                    destinationSpace[1] = '2';
+                    firstClick = 0;
+                }
+                else if (sender == _73)
+                {
+                    destinationSpace[0] = '7';
+                    destinationSpace[1] = '3';
+                    firstClick = 0;
+                }
             }
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space2)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space3)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space4)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space5)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space6)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space7)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space8)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space9)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space10)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space11)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space12)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space13)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space14)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space15)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space16)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space17)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space18)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space19)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space20)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space21)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space22)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space23)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space24)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space25)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space26)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space27)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space28)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space29)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space30)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space31)
-            //{
-
-            //}
-            //else if ((Windows.UI.Xaml.Shapes.Rectangle)e.OriginalSource == Space32)
-            //{
-
-            //}
+           
         }
 
-        private bool CanMoveBeMade(Windows.UI.Xaml.Shapes.Rectangle Start, Windows.UI.Xaml.Shapes.Rectangle End)
+        private bool CanMoveBeMade(char[] Start, char[] End)
         {
             bool result = false;
+            if (northPlayersTurn == true)
+            {
+                int rowStart = (int)Start[0];
+                int colStart = (int)Start[1];
+                int rowEnd = (int)End[0];
+                int colEnd = (int)End[1];
+
+                if (rowStart == 7)
+                {
+                    result = false;
+                }
+            }
+            else if (northPlayersTurn == false)
+            {
+                int rowStart = (int)Start[0];
+                int colStart = (int)Start[1];
+                int rowEnd = (int)End[0];
+                int colEnd = (int)End[1];
+
+                if (rowStart == 0)
+                {
+                    result = false;
+                }
+            }
+
 
             return result;
         }
